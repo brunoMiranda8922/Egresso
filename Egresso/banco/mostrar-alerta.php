e28b2772947831c8a1636b5b82ff08fa8e0e7c10
Ivo Henrique <ivo.henrique.oliveira21@gmail.com>
2018-09-13 23:51:58 -0300

    Menssagem de erro de conexão
    Foi feito um modal e um alert caso não haja conexão com o servidor.
    
    obs: não consegui isolar a tag modal e nem a js dela.

--- Egresso/banco/mostrar-alerta.php
+++ Egresso/banco/mostrar-alerta.php
@@ -1,18 +1,18 @@
-<?php
 
+<?php
 @session_start();
 function mostrarAlerta($tipo){
     if(isset($_SESSION[$tipo])){
 ?>
-   <center><div class="alert-<?= $tipo ?>"> <?=$_SESSION[$tipo]?> </div></center>
+
+
+   <center><div class="alert alert-<?= $tipo ?>" role="alert"> <h4> <small><?=$_SESSION[$tipo]?></small></h4> <a href="./index.php" class="alert-link">Saiba mais</a> </div></center>
+
     <script>
-        setTimeout(function() { document.getElementsByClassName('alert-danger')[0].remove(); }, 3000);
+        setTimeout(function() { document.getElementsByClassName('alert-danger')[0]);
         setTimeout(function() { document.getElementsByClassName('alert-success')[0].remove(); }, 3000);
-    </script> 
-<?php        
+    </script>
+<?php
         unset($_SESSION[$tipo]);
     }
-}
-
-?>  
-
+}?>
