32f0fd6a691718a80f50788e17835b787c228350
Ivo Henrique <ivo.henrique.oliveira21@gmail.com>
2018-09-14 22:39:54 -0300

    Mensagem de erro quando esta sem conexão com o servidor
    Foi feito um modal e um alert na pagina index.php caso não haja conexão com o servidor.

--- banco/conexao.php
+++ banco/conexao.php
@@ -1,3 +1,13 @@
 <?php
-$conexao = mysqli_connect("localhost", "bruno", "", "fatec");
-
+$conexao = mysqli_connect("localhost", "username", "password", "database");
+$_SESSION['danger']= NULL;
+$_SESSION['respostaBD']= NULL;
+if(!$conexao) {
+  $_SESSION['respostaBD']= "conexao_erro";
+  $_SESSION['danger']= "Erro ao conectar com o servidor :";
+  /*echo $_SESSION['resultadoBD'];*/
+}
+/*else {
+    echo "conexao_ok";
+  }*/
+?>
