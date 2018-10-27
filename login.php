7e6f6d6ce630d6a365f68c43d0f84845d4be56aa
Bruno Miranda <bruno.silva@catho.com>
2018-10-27 16:56:37 -0300

    ajuste na chamada da função buscarUsuarios

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
-<?php require_once("banco/conexao.php"); //Função que verifica se existe email ou senha na base de dados
+<?php require_once("banco/conexao.php");
 require_once("banco/banco-usuario.php");
 require_once("banco/verifica-usuario.php");
 require_once("banco/mostrar-alerta.php");
 
-
-$usuario = buscarUsuario($conexao, $_POST['nome'], $_POST['senha']);
-
+$usuario = buscarUsuario($conexao, $_POST['email'], $_POST['senha']);
 
 if ($usuario == null){
     $_SESSION['danger'] = "Email ou Senha invalido";
