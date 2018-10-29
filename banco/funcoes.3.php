9b4804fd7a4ff9583daff8dc80162d1d5f409f09
Bruno Miranda <bruno.silva@catho.com>
2018-10-29 07:59:34 -0300

    removendo espaços em brancos, e ajustando alinhamento

--- banco/funcoes.3.php
+++ banco/funcoes.3.php
@@ -1,34 +1,31 @@
 <?php require_once("banco/conexao.php");
 
-function cadastrarQuestionario($conexao, $trabalha, $empresa, $area, $cargo, $formacao, $cronograma, $professores, $intra, $recomendaria, $sexo, $estagio, $semestre, $egresso_id) 
-{                                           
-    
-    $query = "insert into formulario_aluno(trablha, empresa, area_curso_id, cargo, formacao, cronograma, professores, infraestrutura, recomendaria, sexo, estagio, semestre, egresso_id), 
+function cadastrarQuestionario($conexao, $trabalha, $empresa, $area, $cargo, $formacao, $cronograma, $professores, $intra, $recomendaria, $sexo, $estagio, $semestre, $egresso_id)
+{
+    $query = "insert into formulario_aluno(trablha, empresa, area_curso_id, cargo, formacao, cronograma, professores, infraestrutura, recomendaria, sexo, estagio, semestre, egresso_id),
                 values({$trabalha}, '{$empresa}', '{$area}', {$cargo}, '{$formacao}', '{$cronograma}', '{$professores}',
                     '{$intra}', '{$recomendaria}', '{$sexo}', '{$estagio}', '{$semestre}', {'$egresso_id'})";
     return mysqli_query($conexao, $query);
-    
-    
 }
 
 function alunos($conexao)
 {
-$lista = array();
-$query = "SELECT DISTINCT  f.id, f.RA, f.cpf, f.nome, f.email, c.nome AS curso, ano_formacao AS ano, 
-data_semestre AS semestre, ci.nome AS cidade, f.PR from egressos AS f INNER JOIN cursos AS c ON f.cursos_id = c.id
-    INNER JOIN ano ON f.ano_id = ano.id INNER JOIN semestre ON f.semestre_id = semestre.id INNER JOIN cidade AS ci ON f.cidade_id = ci.id
-        ORDER BY ano_formacao DESC";
-$resultado = mysqli_query($conexao, $query);
-while ($row = mysqli_fetch_assoc($resultado)) {
-   $lista[] = $row;
+    $lista = array();
+    $query = "SELECT DISTINCT f.id, f.RA, f.cpf, f.nome, f.email, c.nome AS curso, ano_formacao AS ano,
+                data_semestre AS semestre, ci.nome AS cidade, f.PR from egressos AS f INNER JOIN cursos AS c ON f.cursos_id = c.id
+                    INNER JOIN ano ON f.ano_id = ano.id INNER JOIN semestre ON f.semestre_id = semestre.id INNER JOIN cidade AS ci ON f.cidade_id = ci.id
+                        ORDER BY ano_formacao DESC";
+    $resultado = mysqli_query($conexao, $query);
+    while ($row = mysqli_fetch_assoc($resultado)) {
+        $lista[] = $row;
     }
-     return $lista;
+    return $lista;
 }
 
-function listarRespostas($conexao) 
+function listarRespostas($conexao)
 {
     $respostas = array();
-    $query = "SELECT e.RA, e.nome, a.nome AS area, f.* FROM formulario_aluno AS f INNER JOIN egressos AS e ON e.id  = f.egressos_id LEFT JOIN area_curso as a ON f.area_trabalho = a.area_curso_id 
+    $query = "SELECT e.RA, e.nome, a.nome AS area, f.* FROM formulario_aluno AS f INNER JOIN egressos AS e ON e.id  = f.egressos_id LEFT JOIN area_curso as a ON f.area_trabalho = a.area_curso_id
                 GROUP BY f.id ORDER BY f.id DESC LIMIT 6";
     $resultado = mysqli_query($conexao, $query);
     while ($row = mysqli_fetch_assoc($resultado))
@@ -38,10 +35,10 @@ function listarRespostas($conexao)
     return $respostas;
 }
 
-function listarTrabalha($conexao) 
+function listarTrabalha($conexao)
 {
     $respostas = array();
-    $pagina          = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
+    $pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
     if(!isset($_GET['trabalha']) || empty($_GET['trabalha'])) {
         $_GET['trabalha'] = '1||trabalha=0';
     }
@@ -51,10 +48,9 @@ function listarTrabalha($conexao)
     $indiferente = $_GET['trabalha'];
     
     $query = "SELECT e.RA, e.nome, a.nome AS area, f.* FROM formulario_aluno AS f INNER JOIN egressos AS e ON e.id  = f.egressos_id LEFT JOIN area_curso as a ON f.area_trabalho = a.area_curso_id
-            WHERE f.trabalha = $nao OR f.trabalha = $sim OR f.trabalha LIKE '$indiferente'  GROUP BY f.id";
+                WHERE f.trabalha = $nao OR f.trabalha = $sim OR f.trabalha LIKE '$indiferente'  GROUP BY f.id";
     $resultado = mysqli_query($conexao, $query);
 
-    
     while ($aluno = mysqli_fetch_assoc($resultado)) {
         $respostas[] = $aluno;
     }
@@ -63,8 +59,8 @@ function listarTrabalha($conexao)
 
 function paginarTrabalha($conexao)
 {
-    $alunos          = array();
-    $pagina          = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
+    $alunos = array();
+    $pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
     if(!isset($_GET['trabalha']) || empty($_GET['trabalha'])) {
         $_GET['trabalha'] = '1||trabalha=0';
     }
@@ -88,9 +84,6 @@ function paginarTrabalha($conexao)
     return $numero_pagina;
     
 }
-
-
-
 
 function listarResposta($conexao) 
 {
