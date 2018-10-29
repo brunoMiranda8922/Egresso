0d1c1373a42813a916a614d1a3874866a25c27b4
Bruno Miranda <bruno.silva@catho.com>
2018-10-29 07:30:32 -0300

    refatorando arquivos

---
+++ banco/inserirDados.php
@@ -0,0 +1,10 @@
+<?php
+
+function inserirAlunoDeNovo($conexao, $RA, $nome, $CPF, $email, $telefone, $foto, $curso_id, $ano_id, $semestre_id, $cidade_id, $matricula_id)
+{
+    $query = "INSERT INTO alunos(RA, nome, cpf, email, telefone, foto, cursos_id, ano_id, semestre_id, cidade_id, matricula_id) 
+                VALUES({$RA}, '{$nome}', '{$CPF}', '{$email}', '{$telefone}', '{$foto}', '{$curso_id}', '{$ano_id}', '{$semestre_id}', '{$cidade_id}', '{$matricula_id}')";
+    return mysqli_query($conexao, $query);
+}
+
+?>
