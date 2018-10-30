2b11943bc3898d724f3e95ca211b92e1294f89d3
Bruno Miranda <bruno.silva@catho.com>
2018-10-29 23:00:23 -0300

    ajustando função que gera o Qrcode

--- banco/funcoes.2.php
+++ banco/funcoes.2.php
@@ -1,28 +1,6 @@
 <?php
 require_once("banco/conexao.php");
 
-function cadastrarQuestionario($conexao, $trabalha, $empresa, $area, $cargo, $formacao, $cronograma, $professores, $intra, $recomendaria, $sexo, $estagio, $semestre, $egresso_id)
-{
-    $query = "insert into formulario_aluno(trablha, empresa, area_curso_id, cargo, formacao, cronograma, professores, infraestrutura, recomendaria, sexo, estagio, semestre, egresso_id), 
-                values({$trabalha}, '{$empresa}', '{$area}', {$cargo}, '{$formacao}', '{$cronograma}', '{$professores}',
-                    '{$intra}', '{$recomendaria}', '{$sexo}', '{$estagio}', '{$semestre}', {'$egresso_id'})";
-    return mysqli_query($conexao, $query);
-}
-
-function alunos($conexao)
-{
-    $lista = array();
-    $query = "SELECT DISTINCT  f.id, f.RA, f.cpf, f.nome, f.email, c.nome AS curso, ano_formacao AS ano,
-                data_semestre AS semestre, ci.nome AS cidade, f.PR from egressos AS f INNER JOIN cursos AS c ON f.cursos_id = c.id
-                    INNER JOIN ano ON f.ano_id = ano.id INNER JOIN semestre ON f.semestre_id = semestre.id INNER JOIN cidade AS ci ON f.cidade_id = ci.id
-                        ORDER BY ano_formacao DESC";
-    $resultado = mysqli_query($conexao, $query);
-    while ($row = mysqli_fetch_assoc($resultado)) {
-        $lista[] = $row;
-    }
-        return $lista;
-}
-
 function listarRespostas($conexao) 
 {
     $respostas = array();
@@ -49,7 +27,7 @@ function listarTrabalha($conexao)
     $indiferente = $_GET['trabalha'];
     
     $query = "SELECT e.RA, e.nome, a.nome AS area, f.* FROM formulario_aluno AS f INNER JOIN egressos AS e ON e.id  = f.egressos_id LEFT JOIN area_curso as a ON f.area_trabalho = a.area_curso_id
-            WHERE f.trabalha = $nao OR f.trabalha = $sim OR f.trabalha LIKE '$indiferente' GROUP BY f.id  ORDER BY f.id DESC ";
+                WHERE f.trabalha = $nao OR f.trabalha = $sim OR f.trabalha LIKE '$indiferente' GROUP BY f.id  ORDER BY f.id DESC ";
     $resultado = mysqli_query($conexao, $query);
 
     
@@ -97,22 +75,20 @@ function listarResposta($conexao)
 function gerarQRCODE($conexao)
 {
     $respostas = array();
-    $query = "SELECT DISTINCT  f.id, f.RA, f.cpf, f.nome, f.email, c.nome AS curso, ano_formacao AS ano,
-                data_semestre AS semestre, ci.nome AS cidade, f.PR from egressos AS f INNER JOIN cursos AS c ON f.cursos_id = c.id
-                    INNER JOIN ano ON f.ano_id = ano.id INNER JOIN semestre ON f.semestre_id = semestre.id INNER JOIN cidade AS ci ON f.cidade_id = ci.id";
+    $query = "SELECT RA, nome, cpf, email, telefone, foto, curso, ano, semestre, cidade, status FROM alunos AS A INNER JOIN cursos AS C ON A.cursos_id = C.id
+                INNER JOIN anos AS AN ON A.anos_id = AN.id INNER JOIN semestres AS S ON A.semestres_id = S.id INNER JOIN cidades AS CI ON A.cidades_id = CI.id
+                    INNER JOIN matricula AS M ON A.matricula_id = M.id ORDER BY ano DESC";
     $resultado = mysqli_query($conexao, $query);
     while ($row = mysqli_fetch_assoc($resultado))
     {
         $respostas[] = $row;
     }
     return $respostas;
-
 }
 
 function qrCode($conexao)
 {
     $code = gerarQRCODE($conexao);
-    
     foreach($code as $resp) {
         $qr =  "qr_img0.50j/php/qr_img.php?";
         $qr .= "d={$resp['nome']},{$resp['RA']},{$resp['curso']},{$resp['cpf']},{$resp['email']}&";
