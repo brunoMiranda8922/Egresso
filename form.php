ac258fccbb878e8b129876077ab2239fd39d5ee6
Bruno Miranda <bruno.silva@catho.com>
2018-10-30 22:58:06 -0300

    ajustando template de cadastro para a nova estrutura

--- form.php
+++ form.php
@@ -9,7 +9,7 @@ $cursos = listarCurso($conexao);
 $anos = listarAno($conexao);
 $semestres = listarSemestre($conexao);
 $cidades = listarCidade($conexao);
-
+$matriculas = listarMatricula($conexao);
 
 ?>
 
@@ -27,10 +27,10 @@ $cidades = listarCidade($conexao);
                                     
                                     
 
-                                        <strong>Formulario de Cadastro</strong> Egressos
+                                        <strong>Formulario de Cadastro</strong> Alunos
                                     </div>
                                     <div class="card-body card-block">
-                                        <form  action="cadastro-aluno.php" id="contact_form" method="POST" enctype="multipart/form-data"  class="form-horizontal" autocomplete="off" onsubmit="return validar()">
+                                        <form  action="cadastro-aluno.php" id="contact_form" method="POST" enctype="multipart/form-data"  class="form-horizontal" autocomplete="off">
                                         <fieldset>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
@@ -42,7 +42,7 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="text-input" class=" form-control-label">RA do Aluno</label>
+                                                    <label for="text-input" class="form-control-label">RA do Aluno</label>
                                                 </div>
                                                 <div class="col-12 col-md-4">
                                                     <input type="text" id="text-input" name="RA" placeholder="Digite o RA do aluno" class="form-control" required maxlength="9" autofocus>
@@ -51,7 +51,7 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="text-input" class=" form-control-label">Nome & Sobrenome</label>
+                                                    <label for="text-input" class="form-control-label">Nome & Sobrenome</label>
                                                 </div>
                                                 <div class="col-12 col-md-9">
                                                     <input type="text" id="text-input" name="nome" placeholder="Digite o nome e sobrenome" class="form-control " required>
@@ -60,7 +60,7 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="text-input" class=" form-control-label">CPF do Aluno</label>
+                                                    <label for="text-input" class="form-control-label">CPF do Aluno</label>
                                                 </div>
                                                 <div class="col-12 col-md-4">
                                                     <input type="text" id="text-input" name="CPF" placeholder="Digite o CPF do aluno" class="form-control" required maxlength="11" autofocus>
@@ -69,26 +69,34 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="email-input" class=" form-control-label">Email</label>
+                                                    <label for="email-input" class="form-control-label">Email</label>
                                                 </div>
                                                 <div class="col-12 col-md-9">
-                                                    <input type="email" id="email-input" name="email" placeholder="Digite o Email" class="form-control" required>
+                                                    <input type="email" id="email-input" name="email" placeholder="Digite o email" class="form-control" required>
                                                     <small class="help-block form-text">Preencha o campo email</small>
                                                 </div>
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="email-input" class=" form-control-label">Percentual de Rendimento</label>
+                                                    <label for="telefone" class=" form-control-label">Telefone</label>
                                                 </div>
                                                 <div class="col-12 col-md-4">
-                                                    <input type="text" id="valor" name="PR" placeholder="PR do Aluno" class="form-control" required maxlength="3">
+                                                    <input type="text" id="telefone" name="telefone" placeholder="Telefone do aluno" class="form-control" required maxlength="11">
                                                     <small class="help-block form-text">Preencha este campo </small>
                                                 </div>
                                             </div>
-
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="select" class=" form-control-label">Curso</label>
+                                                    <label for="valor" class="form-control-label">Foto</label>
+                                                </div>
+                                                <div class="col-12 col-md-9">
+                                                    <input type="file" id="valor" name="foto" placeholder="Telefone do aluno" class="form-control" accept="image/*">
+                                                    <small class="help-block form-text">Inserir foto do aluno </small>
+                                                </div>
+                                            </div>
+                                            <div class="row form-group">
+                                                <div class="col col-md-3">
+                                                    <label for="select" class="form-control-label">Curso</label>
                                                 </div>
                                                 
                                                 <div class="col-12 col-md-9">
@@ -97,7 +105,7 @@ $cidades = listarCidade($conexao);
                                                         <?php foreach($cursos as $curso){ 
                                                             ?>
                                                             <option value="<?= $curso['id'] ?> " required >
-                                                                <?= $curso['nome'] ?>
+                                                                <?= $curso['curso'] ?>
                                                             </option>
                                                             <?php } ?>
                                                     </select>
@@ -106,15 +114,15 @@ $cidades = listarCidade($conexao);
 
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="select" class=" form-control-label">Ano de Formação</label>
+                                                    <label for="select" class="form-control-label">Ano</label>
                                                 </div>
                                                 <div class="col-12 col-md-9">
-                                                    <select name="ano_id" id="select" class="form-control">
+                                                    <select name="anos_id" id="select" class="form-control">
                                                         <option disabled selected>Selecione o Ano</option>
                                                         <?php foreach($anos as $ano){ 
                                                             ?>
                                                             <option value="<?= $ano['id'] ?> ">
-                                                                <?= $ano['ano_formacao'] ?>
+                                                                <?= $ano['ano'] ?>
                                                             </option>
                                                             <?php } ?>
                                                     </select>
@@ -122,15 +130,15 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="select" class=" form-control-label">Semestre</label>
+                                                    <label for="select" class="form-control-label">Semestre</label>
                                                 </div>
                                                 <div class="col-12 col-md-9">
-                                                    <select name="semestre_id" id="select" class="form-control">
+                                                    <select name="semestres_id" id="select" class="form-control">
                                                         <option disabled selected>Selecione o Semestre</option>
                                                         <?php foreach($semestres as $semestre){ 
                                                             ?>
                                                             <option value="<?= $semestre['id'] ?> ">
-                                                                <?= $semestre['data_semestre'] ?>
+                                                                <?= $semestre['semestre'] ?>
                                                             </option>
                                                             <?php } ?>
                                                     </select>
@@ -138,15 +146,31 @@ $cidades = listarCidade($conexao);
                                             </div>
                                             <div class="row form-group">
                                                 <div class="col col-md-3">
-                                                    <label for="select" class=" form-control-label">Cidade do Egresso</label>
+                                                    <label for="select" class="form-control-label">Cidade</label>
                                                 </div>
                                                 <div class="col-12 col-md-9">
-                                                    <select name="cidade_id" id="select" class="form-control" required>
+                                                    <select name="cidades_id" id="select" class="form-control" required>
                                                         <option disabled selected>Selecione a Cidade</option>
                                                         <?php foreach($cidades as $cidade){ 
                                                             ?>
                                                             <option value="<?= $cidade['id'] ?> " required>
-                                                                <?= $cidade['nome'] ?>
+                                                                <?= $cidade['cidade'] ?>
+                                                            </option>
+                                                            <?php } ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="row form-group">
+                                                <div class="col col-md-3">
+                                                    <label for="select" class="form-control-label">Situação da matricula</label>
+                                                </div>
+                                                <div class="col-12 col-md-9">
+                                                    <select name="matricula_id" id="select" class="form-control" required>
+                                                        <option disabled selected>Selecione situação da matricula</option>
+                                                        <?php foreach($matriculas as $matricula){
+                                                            ?>
+                                                            <option value="<?= $matricula['id'] ?> " required>
+                                                                <?= $matricula['status'] ?>
                                                             </option>
                                                             <?php } ?>
                                                     </select>
