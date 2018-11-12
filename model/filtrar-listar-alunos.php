bb837c4547e0570f039870bfb1f415af936ba63d
Bruno Miranda <bruno.silva@catho.com>
2018-11-12 08:47:53 -0200

    correção de bug

--- model/filtrar-listar-alunos.php
+++ model/filtrar-listar-alunos.php
@@ -350,7 +350,7 @@ function paginarlistarAlunos($conexao)
     $valor_pesquisar = (isset($_GET['pesquisar'])) ? $_GET['pesquisar'] : "";
 
     $query = "SELECT RA, nome, cpf, email, telefone, foto, curso, ano, semestre, cidade, status FROM alunos AS A INNER JOIN cursos AS C ON A.cursos_id = C.id
-                INNER JOIN anos AS AN ON A.anos_id = AN.id INNER JOIN semestres AS S ON A.semestres_id = S.id INNER JOIN cidades AS CI ON A.cidadess_id = CI.id
+                INNER JOIN anos AS AN ON A.anos_id = AN.id INNER JOIN semestres AS S ON A.semestres_id = S.id INNER JOIN cidades AS CI ON A.cidades_id = CI.id
                     INNER JOIN matricula AS M ON A.matricula_id = M.id ORDER BY ano DESC";
 
     $resultado_aluno = mysqli_query($conexao, $query);
