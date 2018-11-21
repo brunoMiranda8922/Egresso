025e22482aadf03c87441184c3e17fcb12ef64b5
Bruno Miranda <bruno.silva@catho.com>
2018-11-21 00:29:34 -0200

    ajustando listagem de frequencia

--- model/funcoes.2.php
+++ model/funcoes.2.php
@@ -141,7 +141,7 @@ function listarDadosFrequencia($conexao, $id)
     $query = "SELECT A.id, A.RA, nome, email, curso, status, MONTH(data_entrada) AS MES, DAY(data_entrada) AS DIA, TIME(data_entrada) AS HORARIO FROM alunos AS A
                 INNER JOIN cursos AS C ON A.cursos_id = C.id INNER JOIN anos AS AN ON A.anos_id = AN.id INNER JOIN semestres AS S ON A.semestres_id = S.id
                     INNER JOIN cidades AS CI ON A.cidades_id = CI.id INNER JOIN matricula AS M ON A.matricula_id = M.id RIGHT JOIN frequencia as F ON A.RA = F.RA
-                        WHERE A.id = '{$id}' ORDER BY TIME(data_entrada) DESC";
+                        WHERE A.id = '{$id}' ORDER BY MONTH(data_entrada) DESC, DAY(data_entrada) DESC, TIME(data_entrada) DESC";
     $resultado = mysqli_query($conexao, $query);
     while($row = mysqli_fetch_assoc($resultado))
     {
