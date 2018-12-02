d94747d4651ce5aca6859673ecec8c335442164f
Bruno Miranda <bruno.silva@catho.com>
2018-12-01 23:00:47 -0200

    ajustes error_reporting

--- view/qrcode-alunos.php
+++ view/qrcode-alunos.php
@@ -2,6 +2,7 @@
 require_once("cabecalho.php");
 require_once("../model/conexao.php");
 require_once("../model/funcoes.2.php");
+error_reporting("E_NOTICE");
 qrCode($conexao);
 require_once("rodape.php")
 ?>
